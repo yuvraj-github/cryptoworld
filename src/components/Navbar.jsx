ee65ea70f40c1312596f34d7086239cc222cbd9f
yuvraj <yuvi.unique@gmail.com>
2023-04-01 16:53:39 +0530

    Added app components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,30 @@ import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOut
 import icon from '../images/cryptocurrency.png';
 
 const Navbar = () => {
-  return (
-    <div className='nav-container'>
-        <div className='logo-container'>
-            <Avatar src={icon} size='large' />
-            <Typography.Title level={2} className='logo' >
-                <Link to="/">Cryptoworld</Link>
-            </Typography.Title>
+    return (
+        <div className='nav-container'>
+            <div className='logo-container'>
+                <Avatar src={icon} size='large' />
+                <Typography.Title level={2} className='logo' >
+                    <Link to="/">Cryptoworld</Link>
+                </Typography.Title>
+            </div>
+            <Menu theme='dark'>
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
